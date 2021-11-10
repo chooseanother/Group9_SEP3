d055eaa59e97453d65ca0cd6eb148a04b555965c
Nichita Railean <nichita.railean@aol.com>
2021-11-10 17:10:53 +0200

    Chess Piece added

---
+++ Group9_SEP3_Chess/Models/ChessPiece.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Group9_SEP3_Chess.Models
+{
+    public class ChessPiece
+    {
+        private String loaction { get; set; }
+
+        public String getPiece(String type)
+        {
+            switch (type)
+            {
+                //Black
+                case "BRook":
+                    return "Images/BRook.png";
+                case "BHorse":
+                    return "Images/BHorse.png";
+                case  "BBishop": 
+                    return "Images/BBishop.png";
+                case  "BQueen":
+                    return "Images/BQueen.png";
+                case  "BKing":
+                    return "Images/BKing.png";
+                case "BPawn": 
+                    return "Images/BPawn.png";
+                
+                //White
+                case "WRook": 
+                    return "Images/WRook.png";
+                case "WHorse":
+                    return "Images/WHorse.png";
+                case "WBishop":
+                    return "Images/WBishop.png";
+                case "WQueen":
+                    return "Images/WQueen.png";
+                case "WKing":
+                    return "Images/WKing.png";
+                case "WPawn": 
+                    return "Images/WPawn.png";
+                default:
+                    return null;
+            }
+        } 
+    }
+}
