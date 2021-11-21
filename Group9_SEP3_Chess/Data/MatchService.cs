a26d47e94b4574c440bdb9861ec781c4dcb74b55
ErrorByProject <43143523+ErrorByProject@users.noreply.github.com>
2021-11-21 12:56:08 +0100

    match service uses rabbitmq service

--- Group9_SEP3_Chess/Data/MatchService.cs
+++ Group9_SEP3_Chess/Data/MatchService.cs
@@ -1,71 +1,23 @@
 ﻿using System;
-using System.Collections.Concurrent;
-using System.Text;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using Group9_SEP3_Chess.Json2DArrayHelp;
 using Group9_SEP3_Chess.Models;
-using RabbitMQ.Client;
-using RabbitMQ.Client.Events;
 
 namespace Group9_SEP3_Chess.Data
 {
     public class MatchService:IMatchService
     {
-        private const string QUEUE_NAME = "rpc_queue";
-        private readonly IConnection connection;
-        private readonly IModel channel;
-        private readonly string replyQueueName;
-        private readonly EventingBasicConsumer consumer;
-        private readonly ConcurrentDictionary<string, TaskCompletionSource<string>> callbackMapper =
-            new ConcurrentDictionary<string, TaskCompletionSource<string>>();
+        private IRabbitMQ _rabbitMq;
 
-        public MatchService()
+        public MatchService(IRabbitMQ _rabbitMq)
         {
-            var factory = new ConnectionFactory()
-            {
-                HostName = "localhost"
-            };
-
-            connection = factory.CreateConnection();
-            channel = connection.CreateModel();
-            // declare a server-named queue
-            replyQueueName = channel.QueueDeclare(queue: "").QueueName;
-            consumer = new EventingBasicConsumer(channel);
-            consumer.Received += (model, ea) =>
-            {
-                if (!callbackMapper.TryRemove(ea.BasicProperties.CorrelationId, out TaskCompletionSource<string> tcs))
-                    return;
-                var body = ea.Body.ToArray();
-                var response = Encoding.UTF8.GetString(body);
-                tcs.TrySetResult(response);
-            };
-
-            channel.BasicConsume(
-                consumer: consumer,
-                queue: replyQueueName,
-                autoAck: true);
+            this._rabbitMq = _rabbitMq;
         }
         public async Task<ChessPiece> MoveChessPiece(Message message, CancellationToken cancellationToken = default(CancellationToken))
         {
-            IBasicProperties props = channel.CreateBasicProperties();
-            var correlationId = Guid.NewGuid().ToString();
-            props.CorrelationId = correlationId;
-            props.ReplyTo = replyQueueName;
-            var jsonMessage = JsonSerializer.Serialize(message); 
-            var messageBytes = Encoding.UTF8.GetBytes(jsonMessage);
-            var tcs = new TaskCompletionSource<string>();
-            callbackMapper.TryAdd(correlationId, tcs);
-
-            channel.BasicPublish(
-                exchange: "",
-                routingKey: QUEUE_NAME,
-                basicProperties: props,
-                body: messageBytes);
-
-            cancellationToken.Register(() => callbackMapper.TryRemove(correlationId, out var tmp));
-            Message response = JsonSerializer.Deserialize<Message>(tcs.Task.Result);
+            Message response = await _rabbitMq.SendRequestAsync(message, cancellationToken);
             if (response.Action.Equals("Sending A chess Piece"))
             {
                 ChessPiece chessPiece = JsonSerializer.Deserialize<ChessPiece>(response.Data);
@@ -81,23 +33,7 @@ namespace Group9_SEP3_Chess.Data
 
         public async Task<ChessPiece> UpgradeChessPiece(Message message,CancellationToken cancellationToken = default(CancellationToken))
         {
-            IBasicProperties props = channel.CreateBasicProperties();
-            var correlationId = Guid.NewGuid().ToString();
-            props.CorrelationId = correlationId;
-            props.ReplyTo = replyQueueName;
-            var jsonMessage = JsonSerializer.Serialize(message); 
-            var messageBytes = Encoding.UTF8.GetBytes(jsonMessage);
-            var tcs = new TaskCompletionSource<string>();
-            callbackMapper.TryAdd(correlationId, tcs);
-
-            channel.BasicPublish(
-                exchange: "",
-                routingKey: QUEUE_NAME,
-                basicProperties: props,
-                body: messageBytes);
-
-            cancellationToken.Register(() => callbackMapper.TryRemove(correlationId, out var tmp));
-            Message response = JsonSerializer.Deserialize<Message>(tcs.Task.Result);
+            Message response = await _rabbitMq.SendRequestAsync(message, cancellationToken);
             if (response.Action.Equals("Upgrade Chess Piece"))
             {
                 ChessPiece chessPiece = JsonSerializer.Deserialize<ChessPiece>(response.Data);
@@ -108,24 +44,7 @@ namespace Group9_SEP3_Chess.Data
 
         public async Task<ChessPiece[,]> LoadChessPieces(Message message, CancellationToken cancellationToken = default(CancellationToken))
         {
-            
-            IBasicProperties props = channel.CreateBasicProperties();
-            var correlationId = Guid.NewGuid().ToString();
-            props.CorrelationId = correlationId;
-            props.ReplyTo = replyQueueName;
-            var jsonMessage = JsonSerializer.Serialize(message); 
-            var messageBytes = Encoding.UTF8.GetBytes(jsonMessage);
-            var tcs = new TaskCompletionSource<string>();
-            callbackMapper.TryAdd(correlationId, tcs);
-
-            channel.BasicPublish(
-                exchange: "",
-                routingKey: QUEUE_NAME,
-                basicProperties: props,
-                body: messageBytes);
-
-            cancellationToken.Register(() => callbackMapper.TryRemove(correlationId, out var tmp));
-            Message response = JsonSerializer.Deserialize<Message>(tcs.Task.Result);
+            Message response = await _rabbitMq.SendRequestAsync(message, cancellationToken);
             if (response.Action.Equals("Load ChessBoard"))
             {
                 Console.WriteLine(response.Data);
