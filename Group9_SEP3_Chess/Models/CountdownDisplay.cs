7947ab4ece8f67ad9d5130fe1dfabae5f89a468b
Kim Tranberg <kim.tranberg@gmail.com>
2021-11-29 21:10:30 +0100

    Minor style change

--- Group9_SEP3_Chess/Models/CountdownDisplay.cs
+++ Group9_SEP3_Chess/Models/CountdownDisplay.cs
@@ -24,10 +24,14 @@ namespace Group9_SEP3_Chess.Models
             var hours = TimeLeft % (60 * 60 * 24) / (60 * 60);
             var minutes = TimeLeft % (60*60) / 60;
             var seconds = TimeLeft % 60;
-            return $"Time left {(days>0?(days > 1 ? $"{days} Days" : $"{days} Day"):"")} " +
-                   $"{(hours>0?(hours > 1 ? $"{hours} Hours" : $"{hours} Hour"):"")} " +
-                   $"{(minutes>0?(minutes>1?$"{minutes} Minutes":$"{minutes} Minute"):"")}" +
-                   $" {seconds}";
+            if (seconds > 0)
+            {
+                return $"Time left {(days > 0 ? (days > 1 ? $"{days} days" : $"{days} day") : "")} " +
+                       $"{(hours > 0 ? (hours > 1 ? $"{hours} hours" : $"{hours} hour") : "")} " +
+                       $"{(minutes > 0 ? (minutes > 1 ? $"{minutes} minutes" : $"{minutes} minute") : "")}" +
+                       $"{seconds} seconds";
+            }
+            return "";
         }
     }
 }
