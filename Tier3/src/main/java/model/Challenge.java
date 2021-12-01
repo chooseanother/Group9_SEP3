49bcd7495b0cf502b18ad237f794df34d014ef48
Nichita Railean <nichita.railean@aol.com>
2021-12-01 14:40:26 +0200

    Some comments

--- Tier3/src/main/java/model/Challenge.java
+++ Tier3/src/main/java/model/Challenge.java
@@ -1,5 +1,10 @@
 package model;
 
+/**
+ * @author group9
+ * @version 1.0
+ */
+
 import java.io.Serializable;
 
 public class Challenge implements Serializable {
@@ -7,32 +12,62 @@ public class Challenge implements Serializable {
     private String challenged;
     private int turnTime;
 
+    /**
+     * Creating a Challenge
+     * @param challenger challenger
+     * @param challenged challenged
+     * @param turnTime turn time
+     */
     public Challenge(String challenger, String challenged, int turnTime) {
         this.challenger = challenger;
         this.challenged = challenged;
         this.turnTime = turnTime;
     }
 
+    /**
+     * setts the challenger
+     * @param challenger challenger
+     */
     public void setChallenger(String challenger) {
         this.challenger = challenger;
     }
 
+    /**
+     * setts the challenged
+     * @param challenged challenger
+     */
     public void setChallenged(String challenged) {
         this.challenged = challenged;
     }
 
+    /**
+     * setts turn time
+     * @param turnTime turn time
+     */
     public void setTurnTime(int turnTime) {
         this.turnTime = turnTime;
     }
 
+    /**
+     * Returns the challenger
+     * @return challenger
+     */
     public String getChallenger() {
         return challenger;
     }
 
+    /**
+     * Returns the challenged
+     * @return challenged
+     */
     public String getChallenged() {
         return challenged;
     }
 
+    /**
+     * Returns the turn time
+     * @return turn time
+     */
     public int getTurnTime() {
         return turnTime;
     }
--- Tier3/src/main/java/model/Match.java
+++ Tier3/src/main/java/model/Match.java
@@ -2,6 +2,11 @@ package model;
 
 import java.io.Serializable;
 
+/**
+ * @author group9
+ * @version 1.0
+ */
+
 public class Match implements Serializable {
 
     private int matchID;
@@ -14,6 +19,19 @@ public class Match implements Serializable {
     private Participant whitePlayer;
     private Participant blackPlayer;
 
+
+    /**
+     * Creates a match
+     * @param matchID match id
+     * @param tournamentID tournament id
+     * @param turnTime turn time
+     * @param type type
+     * @param finished status
+     * @param usersTurn user turn
+     * @param latestMove last move
+     * @param whitePlayer white player
+     * @param blackPlayer black player
+     */
     public Match(int matchID, int tournamentID, int turnTime, String type, boolean finished, String usersTurn, long latestMove, Participant whitePlayer, Participant blackPlayer) {
         this.matchID = matchID;
         this.tournamentID = tournamentID;
@@ -26,6 +44,17 @@ public class Match implements Serializable {
         this.blackPlayer = blackPlayer;
     }
 
+    /**
+     * Creates a match
+     *
+     * @param matchID      match id
+     * @param tournamentID tournament id
+     * @param turnTime     turn time
+     * @param type         type
+     * @param finished     status
+     * @param usersTurn    user turn
+     * @param latestMove   last move
+     */
     public Match(int matchID, int tournamentID, int turnTime, String type, boolean finished, String usersTurn, long latestMove) {
         this.matchID = matchID;
         this.tournamentID = tournamentID;
@@ -36,74 +65,146 @@ public class Match implements Serializable {
         this.latestMove = latestMove;
     }
 
+    /**
+     * Returns the Match ID
+     * @return Match ID
+     */
     public int getMatchID() {
         return matchID;
     }
 
+    /**
+     * Sets the Match ID
+     * @param matchID Match ID
+     */
     public void setMatchID(int matchID) {
         this.matchID = matchID;
     }
 
+    /**
+     * Returns the Tournament ID
+     * @return Tournament ID
+     */
     public int getTournamentID() {
         return tournamentID;
     }
 
+    /**
+     * Sets the Tournament ID
+     * @param tournamentID Tournament ID
+     */
     public void setTournamentID(int tournamentID) {
         this.tournamentID = tournamentID;
     }
 
+    /**
+     * Returns the Turn Time
+     * @return Turn Time
+     */
     public int getTurnTime() {
         return turnTime;
     }
 
+    /**
+     * Sets the Turn Time
+     * @param turnTime Turn time
+     */
     public void setTurnTime(int turnTime) {
         this.turnTime = turnTime;
     }
 
+    /**
+     * Returns the type
+     * @return type
+     */
     public String getType() {
         return type;
     }
 
+    /**
+     * Sets the Type
+     * @param type type
+     */
     public void setType(String type) {
         this.type = type;
     }
 
+    /**
+     * Returns if the match is finished
+     * @return match status
+     */
     public boolean getFinished() {
         return finished;
     }
 
+    /**
+     * Sets the match to finished
+     * @param finished finished
+     */
     public void setFinished(boolean finished) {
         this.finished = finished;
     }
 
+    /**
+     * Gets the who's turn it is
+     * @return users turn
+     */
     public String getUsersTurn() {
         return usersTurn;
     }
 
+    /**
+     * Sets the users turn
+     * @param usersTurn users turn
+     */
     public void setUsersTurn(String usersTurn) {
         this.usersTurn = usersTurn;
     }
 
+    /**
+     * gets the latest move
+     * @return latest move
+     */
     public long getLatestMove() {
         return latestMove;
     }
 
+    /**
+     * Sets the Latest Move
+     * @param latestMove latest move
+     */
     public void setLatestMove(long latestMove) {
         this.latestMove = latestMove;
     }
 
+    /**
+     * Returns the White Player
+     * @return
+     */
     public Participant getWhitePlayer() {
         return whitePlayer;
     }
 
+    /**
+     * Sets the white player
+     * @param whitePlayer white player
+     */
     public void setWhitePlayer(Participant whitePlayer) {
         this.whitePlayer = whitePlayer;
     }
 
+    /**
+     * Returns the Black Player
+     * @return black player
+     */
     public Participant getBlackPlayer() {
         return blackPlayer;
     }
 
+    /**
+     * Sets the black player
+     * @param blackPlayer black player
+     */
     public void setBlackPlayer(Participant blackPlayer) {
         this.blackPlayer = blackPlayer;
     }
