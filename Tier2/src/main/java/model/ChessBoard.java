18d213e0aeab0d755b35832e45c2c497f61c6796
Kim Tranberg <kim.tranberg@gmail.com>
2021-12-01 10:09:07 +0100

    Try to move mail sending

--- Tier2/src/main/java/model/ChessBoard.java
+++ Tier2/src/main/java/model/ChessBoard.java
@@ -88,13 +88,13 @@ public class ChessBoard {
                     if(participants.get(i).getUsername().equals(username) && participants.get(i).getColor().equals(selected.getColor())) {
                         if (iTier2RMIClient.MovePiece(selected, matchID) && iTier2RMIClient.UpdateMatchUserTurn(matchID, turnColor)) {
                             //Sending of email after making the turn
-                            for (Participant p : participants) {
-                                if (p.getColor().equals(turnColor)) {
-                                    User user = iTier2RMIClient.getUser(p.getUsername());
-                                    Email email = new Email();
-                                    email.sendEmail("NotSoBusyChess@gmail.com", user.getEmail(), user.getUsername(), matchID);
-                                }
-                            }
+//                            for (Participant p : participants) {
+//                                if (p.getColor().equals(turnColor)) {
+//                                    User user = iTier2RMIClient.getUser(p.getUsername());
+//                                    Email email = new Email();
+//                                    email.sendEmail("NotSoBusyChess@gmail.com", user.getEmail(), user.getUsername(), matchID);
+//                                }
+//                            }
                             testForNullRMI = true;
                         }
                     }
